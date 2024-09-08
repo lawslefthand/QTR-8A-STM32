41f48cce052439198661c037b3bf9ddf48df7e77
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2024-09-09 02:36:54 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "stm32f030x8.h"
+#include <stdio.h>
+#include "qtr_8rc.h"
+#include "usart.h"
+
+int main()
+{
+	usart_gpio_init();
+	usart_init();
+	clk_enable();
+	gpio_config();
+	adc_config();
+
+	while(1)
+	{
+
+        adc_enable();
+		conv_start1();
+		adc_disable();
+
+		adc_enable();
+		conv_start2();
+		adc_disable();
+
+		adc_enable();
+	    conv_start3();
+	    adc_disable();
+
+	    adc_enable();
+		conv_start4();
+		adc_disable();
+
+		adc_enable();
+	    conv_start5();
+	    adc_disable();
+
+	    adc_enable();
+		conv_start6();
+		adc_disable();
+
+	}
+}
