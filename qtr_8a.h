f4a1d25cb27417dc2ee442cab068c622b7ef08d3
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2024-09-09 02:38:11 +0530

    Add files via upload

---
+++ qtr_8a.h
@@ -0,0 +1,25 @@
+/*
+ * qrt_8rc.h
+ *
+ *  Created on: Sep 8, 2024
+ *      Author: danba
+ */
+
+#ifndef SRC_QTR_8A_H_
+#define SRC_QTR_8A_H_
+
+
+// Function prototypes
+void clk_enable(void);
+void gpio_config(void);
+void adc_enable(void);
+void adc_disable(void);
+void adc_config(void);
+void conv_start1(void);
+void conv_start2(void);
+void conv_start3(void);
+void conv_start4(void);
+void conv_start5(void);
+void conv_start6(void);
+
+#endif /* SRC_QTR_8A_H_ */
