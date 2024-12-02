fb43730f1182779594c502a6ef7bc07b71c11bee
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2024-12-02 13:41:29 +0530

    Update qtr_8a.h

--- qtr_8a.h
+++ qtr_8a.h
@@ -8,8 +8,12 @@
 #ifndef SRC_QTR_8A_H_
 #define SRC_QTR_8A_H_
 
+// RCC Register Definitions for GPIO and ADC
+#define RCC_AHBENR_GPIOAEN      (1U << 17) // Enable GPIOA clock
+#define RCC_AHBENR_GPIOBEN      (1U << 18) // Enable GPIOB clock
+#define RCC_AHBENR_GPIOCEN      (1U << 19) // Enable GPIOC clock
+#define RCC_APB2ENR_ADC1EN      (1U << 9)  // Enable ADC1 clock
 
-// Function prototypes
 void clk_enable(void);
 void gpio_config(void);
 void adc_enable(void);
@@ -21,5 +25,7 @@ void conv_start3(void);
 void conv_start4(void);
 void conv_start5(void);
 void conv_start6(void);
+
+
 
 #endif /* SRC_QTR_8A_H_ */
